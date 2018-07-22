dc0044050c156a43447044f1c7db4f62af08d1e0
Jonathan Abraham <jonathan.abraham@outlook.com>
2018-07-22 09:09:22 +0200

    feat: Init back projects

---
+++ back/VideoStore/VideoStore.Business/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace VideoStore.Business.Models
+{
+    public class Movie
+    {
+        public const int REGULAR     = 0;
+        public const int NEW_RELEASE = 1;
+        public const int CHILDREN    = 2;
+
+        public int PriceCode { get; set; }
+        public virtual string Title { get; }
+
+        public Movie(string title, int priceCode)
+        {
+            this.Title     = title;
+            this.PriceCode = priceCode;
+        }
+    }
+}
---
+++ back/VideoStore/VideoStore.Business/Models/Rental.cs
@@ -0,0 +1,14 @@
+﻿namespace VideoStore.Business.Models
+{
+    public class Rental
+    {
+        public int DaysRented { get; }
+        public virtual Movie Movie { get; }
+
+        public Rental(Movie movie, int daysRented)
+        {
+            this.Movie      = movie;
+            this.DaysRented = daysRented;
+        }
+    }
+}
